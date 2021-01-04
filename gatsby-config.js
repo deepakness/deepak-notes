f26bd04578d782964193b73f3afc6887fa7deb22
deepakness <deepakkr342@gmail.com>
2021-01-04 10:03:37 +0530

    init

---
+++ gatsby-config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: 'Deepak\'s Notes',
+    siteDescription: 'Notes about blogging, marketing, business, technology & more...',
+    // siteImage: '/banner.png', // main image of the site for metadata
+    // siteUrl: 'https://chronoblog.now.sh/',
+    pathPrefix: '/',
+    siteLanguage: 'en',
+    ogLanguage: `en_US`,
+    author: 'Deepak K', // for example - 'Deepak K'
+    authorDescription: 'I write and develop business strategies.', // short text about the author
+    avatar: '/avatar.jpg',
+    twitterSite: '', // website account on twitter
+    twitterCreator: '', // creator account on twitter
+    social: [
+      {
+        icon: `at`,
+        url: `mailto:deepak@deepakness.com`
+      },
+      {
+        icon: `twitter`,
+        url: `https://twitter.com/DeepakNesss`
+      },
+      {
+        icon: `github`,
+        url: `https://github.com/deepakness`
+      }
+    ]
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-theme-chronoblog',
+      options: {
+        uiText: {
+          // ui text fot translate
+          feedShowMoreButton: 'show more',
+          feedSearchPlaceholder: 'search',
+          cardReadMoreButton: 'read more →',
+          allTagsButton: 'all tags'
+        },
+        feedItems: {
+          // global settings for feed items
+          limit: 50,
+          yearSeparator: true,
+          yearSeparatorSkipFirst: true,
+          contentTypes: {
+            links: {
+              beforeTitle: '🔗 '
+            }
+          }
+        },
+        feedSearch: {
+          symbol: '🔍'
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Chronoblog Gatsby Theme`,
+        short_name: `Chronoblog`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#3a5f7d`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: 'UA-XXXXXXXXX-X'
+      }
+    }
+  ]
+};
